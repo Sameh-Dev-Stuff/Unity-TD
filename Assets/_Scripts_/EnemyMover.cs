b4a2b629c9f4f652344d88f4e08756eedb7a1d35
Sameh Hamidi <khalidhamidi525@gmail.com>
2026-08-12 09:36:18 +0330

    Enemy Movement implemented

---
+++ Assets/_Scripts_/EnemyMover.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using DG.Tweening;
+using NaughtyAttributes;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private Transform[] pathPoints;
+    [SerializeField] private float speed = 2f;
+
+    [SerializeField,ReadOnly] private int index;
+    
+    [Header("Ease")]
+    [SerializeField] private Ease ease = Ease.Linear;
+
+    private void Start()
+    {
+        MoveToNextPoint();
+    }
+
+    private void MoveToNextPoint()
+    {
+        if (index >= pathPoints.Length)
+        {
+            ReachBase();
+            return;
+        }
+
+        Transform target = pathPoints[index];
+
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        float duration = distance / speed;
+
+        transform.DOMove(target.position, duration).SetEase(ease).OnComplete(() =>
+        {
+            
+            index++;
+            MoveToNextPoint();
+            
+        });
+    }
+
+    private void ReachBase()
+    {
+        Debug.Log("Enemy reached the base!");
+        Destroy(gameObject);
+    }
+}
